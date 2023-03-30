db6c6169c90b252354464346b9c6269ecea44496
Gino Lee <ginolee859@gmail.com>
2023-03-30 17:47:36 -0400

    wip

--- work2/src/app/app.component.ts
+++ work2/src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'my-app';
+
+  title$ = new Observable((observer) => {
+    setInterval(() => {
+      observer.next();
+    }, 2000);
+  });
+
+  private setTitle = () => {
+    console.log('invoked setTitle lambda');
+    const d = new Date();
+    let timestamp = d.getSeconds() + '.' + d.getMilliseconds();
+    console.log(`new timestamp = ${timestamp}`);
+    this.title = `Learning Angular (${timestamp})`;
+  };
+
+  private onComplete() {
+    return new Promise<void>((resolve) => {
+      setInterval(() => {
+        resolve();
+      }, 2000);
+    });
+  }
+
+  private changeTitle(callback: Function) {
+    setTimeout(() => {
+      callback();
+    }, 2000);
+  }
+
+  constructor() {
+    this.title$.subscribe(this.setTitle);
+  }
 }
