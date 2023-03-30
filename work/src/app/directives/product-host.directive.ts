8a48ae7627f204f155a59c639d9f35e4f9d51a9c
Gino Lee <ginolee859@gmail.com>
2023-03-30 11:00:25 -0400

    wip

---
+++ work/src/app/directives/product-host.directive.ts
@@ -0,0 +1,17 @@
+import { Directive, OnInit, ViewContainerRef } from '@angular/core';
+import { ProductDetailComponent } from '../products/product-detail/product-detail.component';
+
+@Directive({
+  selector: '[appProductHost]',
+})
+export class ProductHostDirective implements OnInit {
+  constructor(private vc: ViewContainerRef) {}
+
+  ngOnInit(): void {
+    const productRef = this.vc.createComponent(ProductDetailComponent);
+    productRef.setInput('product', {
+      name: 'Optical mouse',
+      price: 130,
+    });
+  }
+}
