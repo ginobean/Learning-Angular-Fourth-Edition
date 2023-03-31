0bd595223520d3904a0a4093acfa32f09e546ad2
Gino Lee <ginolee859@gmail.com>
2023-03-31 09:02:44 -0400

    wip

--- work2/src/app/key-logger/key-logger.component.ts
+++ work2/src/app/key-logger/key-logger.component.ts
@@ -4,9 +4,10 @@ import {
   OnDestroy,
   OnInit,
   ViewChild,
+  Input,
 } from '@angular/core';
 import { Observable, Subscription, fromEvent } from 'rxjs';
-import { tap } from 'rxjs';
+import { tap, map, filter } from 'rxjs';
 
 @Component({
   selector: 'app-key-logger',
@@ -18,14 +19,24 @@ export class KeyLoggerComponent implements OnInit, OnDestroy {
   keys = '';
   subscription: Subscription | undefined;
 
+  @Input() numeric = false;
+
   ngOnInit(): void {
     const logger$ = fromEvent<KeyboardEvent>(
       this.input?.nativeElement,
       'keyup'
     );
     this.subscription = logger$
-      .pipe(tap((evt) => console.log('received key = ' + evt.key)))
-      .subscribe((evt) => (this.keys += evt.key));
+      .pipe(
+        map((evt) => evt.key.charCodeAt(0)),
+        filter(
+          (code) =>
+            (this.numeric && code >= 48 && code <= 57) ||
+            (!this.numeric && code > 31)
+        ),
+        tap((code) => console.log('received keycode = ' + code))
+      )
+      .subscribe((code) => (this.keys += String.fromCharCode(code)));
   }
 
   ngOnDestroy(): void {
