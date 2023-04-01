582a0b890c68b30ab8e5284bd8f90342d1a49b94
Gino Lee <ginolee859@gmail.com>
2023-04-01 09:22:32 -0400

    ng assumes that modules are located in same/parent directory (not sibling directory)

--- work/src/app/app.module.ts
+++ work/src/app/app.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { HttpClientModule } from '@angular/common/http';
-import { AppComponent } from '../components/app.component';
-import { CopyrightDirective } from '../directives/copyright.directive';
-import { NumericDirective } from '../directives/numeric.directive';
-import { PermissionDirective } from '../directives/permission.directive';
+import { AppComponent } from './components/app.component';
+import { CopyrightDirective } from './directives/copyright.directive';
+import { NumericDirective } from './directives/numeric.directive';
+import { PermissionDirective } from './directives/permission.directive';
 import { ProductsModule } from './products.module';
 
 @NgModule({
