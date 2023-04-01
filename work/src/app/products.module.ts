3ff4c404110dde265ffbb47be342911e845311b9
Gino Lee <ginolee859@gmail.com>
2023-04-01 09:51:35 -0400

    wip

--- work/src/app/products.module.ts
+++ work/src/app/products.module.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FavoritesComponent } from './components/favorites.component';
+import { ProductCreateComponent } from './components/product-create.component';
 import { ProductDetailComponent } from './components/product-detail.component';
 import { ProductListComponent } from './components/product-list.component';
 import { ProductViewComponent } from './components/product-view.component';
 import { ProductHostDirective } from './directives/product-host.directive';
 import { SortProductsPipe } from './pipes/sort-products.pipe';
-import { ProductCreateComponent } from './components/product-create.component';
 
 @NgModule({
   declarations: [
