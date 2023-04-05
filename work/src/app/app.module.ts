4ff71ae013fd115b483c95949499df43b7abd8ed
Gino Lee <ginolee859@gmail.com>
2023-04-05 18:29:11 -0400

    playing with some material components

--- work/src/app/app.module.ts
+++ work/src/app/app.module.ts
@@ -2,16 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { AuthModule } from './auth.module';
 import { AppComponent } from './components/app.component';
+import { AuthComponent } from './components/auth.component';
+import { PageNotFoundComponent } from './components/page-not-found.component';
 import { CopyrightDirective } from './directives/copyright.directive';
 import { NumericDirective } from './directives/numeric.directive';
 import { PermissionDirective } from './directives/permission.directive';
-import { ProductsModule } from './products.module';
-import { AuthModule } from './auth.module';
-import { AuthComponent } from './components/auth.component';
 import { AuthInterceptor } from './interceptors/auth.interceptor';
-import { AppRoutingModule } from './app-routing.module';
-import { PageNotFoundComponent } from './components/page-not-found.component';
+import { ProductsModule } from './products.module';
+import { MatCommonModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [
@@ -22,7 +28,18 @@ import { PageNotFoundComponent } from './components/page-not-found.component';
     PermissionDirective,
     PageNotFoundComponent,
   ],
-  imports: [BrowserModule, ProductsModule, HttpClientModule, AuthModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    ProductsModule,
+    HttpClientModule,
+    AuthModule,
+    AppRoutingModule,
+    MatIconModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCommonModule,
+    BrowserAnimationsModule,
+  ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
   ],
--- work/src/app/products.module.ts
+++ work/src/app/products.module.ts
@@ -1,5 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { RouterModule } from '@angular/router';
 import { CartComponent } from './components/cart.component';
 import { FavoritesComponent } from './components/favorites.component';
@@ -9,7 +13,6 @@ import { ProductListComponent } from './components/product-list.component';
 import { ProductViewComponent } from './components/product-view.component';
 import { ProductsComponent } from './components/products.component';
 import { SortProductsPipe } from './pipes/sort-products.pipe';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -22,7 +25,15 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     CartComponent,
     ProductsComponent,
   ],
-  imports: [CommonModule, RouterModule, FormsModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   exports: [ProductListComponent],
 })
 export class ProductsModule {}
