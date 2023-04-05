8746ce54662e376066673795d69f5a92200db328
Gino Lee <ginolee859@gmail.com>
2023-04-05 11:11:37 -0400

    add patchValue example. iterate over elements of a javascript/typescript object, in the html template

--- forms-array-app/src/app/forms-array-example.component.ts
+++ forms-array-app/src/app/forms-array-example.component.ts
@@ -7,14 +7,18 @@ import { FormArray, FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./forms-array-example.component.css'],
 })
 export class FormsArrayExampleComponent {
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder) {
+    console.log('typeof editform.controls = ' + typeof this.editForm.controls);
+  }
 
   editForm = this.fb.group({
     companyName: ['', Validators.required],
+    rowCount: [0],
     lessons: this.fb.array([]),
   });
 
   get lessons() {
+    // this.editForm.controls.
     return this.editForm.controls['lessons'] as FormArray;
   }
 
@@ -25,12 +29,15 @@ export class FormsArrayExampleComponent {
     });
 
     this.lessons.push(lessonForm);
+
+    this.editForm.patchValue({ rowCount: this.lessons.length });
   }
 
   deleteLesson() {
     const sz = this.lessons.length;
     if (sz > 0) {
       this.lessons.removeAt(sz - 1);
+      this.editForm.patchValue({ rowCount: this.lessons.length });
     }
   }
 }
